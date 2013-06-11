c1a42a64acb0304371523b1f9aefc39a4095adbd
Stuart Blair <stuart.a.blair@gmail.com>
2013-06-11 05:06:52 +0000

    Still refactoring on green. Let's actually instansiate the TemporalLocality class and start to put some meat on these bones.

--- lib/temporal_locality.rb
+++ lib/temporal_locality.rb
@@ -1,9 +1,16 @@
 require "temporal_locality/version"
 
 module TemporalLocality
-  puts """
+	class TemporalLocality
+		def report
+puts """
 | resource |   | 1   | 2   |
 | file_a   | 1 | x   | 100 |
 | file_b   | 2 | 100 | x   |
 """
+		end
+	end
 end
+
+TemporalLocality::TemporalLocality.new.report
+
