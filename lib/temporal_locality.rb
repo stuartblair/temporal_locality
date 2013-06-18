9efb24d74944a9bb57d23e88d2ed3aad06107337
Stuart Blair <stuart.a.blair@gmail.com>
2013-06-18 18:58:05 +0000

    Introduced the render method as my first step in removing the duplication between our implementation and our test. Even the journey of a thousand refactorings begins with a single commit.

--- lib/temporal_locality.rb
+++ lib/temporal_locality.rb
@@ -3,6 +3,11 @@ require "temporal_locality/version"
 module TemporalLocality
 	class TemporalLocality
 		def report
+			render_view
+			
+		end
+
+		def render_view
 puts """
 | resource |   | 1   | 2   |
 | file_a   | 1 | x   | 100 |
